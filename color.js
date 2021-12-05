45c42f1d7b248b963964f748a73191edf5a1640d
Amita  chorghe <67407962+amitachorghe@users.noreply.github.com>
2021-12-05 19:29:30 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,13 @@
+const colors = ["#4287f5", "#4287f5", "#f57842", "#f542cb","#b042f5", "#008000",];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",  () => {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
